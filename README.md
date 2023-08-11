35dc3a7ef88d5c72ef42b2e0498a4e7ca243c12d
superandybean <andrewlamm04@gmail.com>
2023-08-11 00:47:37 -0400

    update info

--- README.md
+++ README.md
@@ -4,8 +4,13 @@ my [personal site](https://andrewl.dev/)
 
 ### how to run
 
-1. `git clone https://github.com/superandybean/personal-site.git`
+1. `git clone git@github.com:superandybean/personal-site.git`
 2. `cd personal-site`
 3. `npm install`
 4. `gatsby develop`
 5. visit site on `localhost:8000`
+
+### to deploy
+
+make sure you are logged in with `firebase login`
+`firebase deploy`
--- src/pages/index.js
+++ src/pages/index.js
@@ -18,6 +18,12 @@ import HeaderLink from '../components/headerLink'
 import ProjectCard from '../components/projectCard'
 import SEO from '../components/seo'
 
+const OutsideLink = ({ children, ...props }) => (
+  <Themed.a {...props} target='_blank' rel='noopener noreferrer'>
+    {children}
+  </Themed.a>
+)
+
 const IndexPage = ({ data }) => {
   const {
     express: { childImageSharp: expressImg },
@@ -60,6 +66,17 @@ const IndexPage = ({ data }) => {
 
   const projects = [
     {
+      name: 'CSGODOKU',
+      fullname: 'CSGODOKU',
+      img: undefined,
+      desc: 'An immaculate grid game for CS:GO. Immaculate Grid is a game featuring a 3x3 board with a clue corresponding to each row and column. \
+      The user\'s goal is to select a player in each grid spot such that the clue for each row and column in that grid spot is satisfied without repeating any players. \
+      Data from the CS:GO players are scraped from HLTV using JavaScript and the puzzles are generated using a python script and are stored on a MongoDB database. \
+      The site is built with Express and EJS and is stored on Render.',
+      link: 'https://csgodoku.onrender.com/',
+      github: 'https://github.com/superandybean/csgodoku',
+    },
+    {
       name: 'Learning the Express Way',
       fullname: 'Learning the Express Way',
       img: expressImg,
@@ -70,15 +87,26 @@ const IndexPage = ({ data }) => {
       github: 'https://github.com/superandybean/learning-the-express-way',
     },
     {
-      name: 'Dota Predictions Game',
-      fullname: 'Dota Predictions Game',
+      name: 'CS:GO Predictions Game',
+      fullname: 'CS:GO Predictions Game',
       img: dotaImg,
-      desc: 'A predicting game inspired by FiveThirtyEight\'s NFL Forecasting game. On the site, users predict the outcome of professional Dota 2 matches and earn/lose points based on how confident their predictions are. \
-      A more confident correct pick earns more points than a less confident correct pick, but a confident incorrect pick loses more points than a less confident one! The site was built with Express and Handlebars, with data being stored in a MongoDB database. \
-      The site originally was built for predicting Dota matches but also supports predictions on CS:GO games.',
-      note: 'The site was originally hosted on Heroku, but is currently down due to the end of the Dota 2 season.',
-      github: 'https://github.com/superandybean/prediction-game',
+      desc: 'A predicting game inspired by FiveThirtyEight\'s NFL Forecasting game. On the site, users predict the outcome of professional CS:GO matches and earn/lose points based on how confident their predictions are. \
+      A more confident correct pick earns more points than a less confident correct pick, but a confident incorrect pick loses more points than a less confident one! \
+      Users can log in using their Steam account on the website through the Steam OpenID library. Users can also compare their picks with other users through the leaderboard feature. Custom leaderboards are also a upcoming planned feature. \
+      The site was built with Express and EJS with user data being stored in a MongoDB database. ',
+      note: 'The site was originally hosted on Render, but is currently down to host my other project, CSGODOKU.',
+      github: 'https://github.com/superandybean/predictions-game',
     },
+    // {
+    //   name: 'Dota Predictions Game',
+    //   fullname: 'Dota Predictions Game',
+    //   img: dotaImg,
+    //   desc: 'A predicting game inspired by FiveThirtyEight\'s NFL Forecasting game. On the site, users predict the outcome of professional Dota 2 matches and earn/lose points based on how confident their predictions are. \
+    //   A more confident correct pick earns more points than a less confident correct pick, but a confident incorrect pick loses more points than a less confident one! The site was built with Express and Handlebars, with data being stored in a MongoDB database. \
+    //   The site originally was built for predicting Dota matches but also supports predictions on CS:GO games.',
+    //   note: 'The site was originally hosted on Heroku, but is currently down due to the end of the Dota 2 season.',
+    //   github: 'https://github.com/superandybean/predictions-game',
+    // },
     {
       name: 'RateMyTJ',
       fullname: 'RateMyTJ',
@@ -159,14 +187,14 @@ const IndexPage = ({ data }) => {
       note: 'Unfortunately, due to Honor Code violations, code for crossword generation is not public. However, the code for the GUI is available and found in the link below. Instructions for running the program with your own crossword generator are also provided in the link below.',
       github: 'https://github.com/superandybean/personal-site/tree/master/static/program_code',
     },
-    {
-      name: 'College Management System',
-      fullname: 'College & University Management System',
-      img: cumsImg,
-      desc: 'The College & University Management System is a web application that was built for HackTJ 9.0. The site allows users to easily manage their college applications-from a calendar of deadlines to an easy way to keep track of all of their essays. \
-      The site was built with Express and Handlebars and uses the Department of Education\'s API to obtain college information including average cost and SAT score. Unfortunately, not all of the planned features were implemented.',
-      github: 'https://github.com/superandybean/college-university-managment-system',
-    },
+    // {
+    //   name: 'College Management System',
+    //   fullname: 'College & University Management System',
+    //   img: cumsImg,
+    //   desc: 'The College & University Management System is a web application that was built for HackTJ 9.0. The site allows users to easily manage their college applications-from a calendar of deadlines to an easy way to keep track of all of their essays. \
+    //   The site was built with Express and Handlebars and uses the Department of Education\'s API to obtain college information including average cost and SAT score. Unfortunately, not all of the planned features were implemented.',
+    //   github: 'https://github.com/superandybean/college-university-managment-system',
+    // },
     {
       name: 'Sudoku Solver',
       fullname: 'Sudoku Solver',
@@ -415,14 +443,25 @@ const IndexPage = ({ data }) => {
               about me
             </motion.div>
             <ContentCard>
-              I'm Andrew Lam, a freshman studying Computer Science at Carnegie Mellon University. My classes this semester include <Themed.a href="https://www.math.cmu.edu/~jmackey/151_128/welcome.html" target='_blank' rel='noopener noreferrer'>Mathematical Foundations in Computer Science</Themed.a> and <Themed.a href="http://www.cs.cmu.edu/~15122/" target='_blank' rel='noopener noreferrer'>Principles of Imperative Computation</Themed.a>.
-              The Computer Science topics that interest me the most currently are web development and artificial intelligence. Outside of class, I enjoy gaming and relaxing.
+              I'm Andrew Lam, a sophomore studying Computer Science at Carnegie Mellon University. My classes this semester include <OutsideLink href="https://www.cs.cmu.edu/~213/">Introduction to Computer Systems</OutsideLink> and <OutsideLink href="https://www.cs.cmu.edu/~crary/317-f23/">Constructive Logic</OutsideLink>.
+              Currently, I am currently a Teaching Assistant for <OutsideLink href="http://www.cs.cmu.edu/~15150/">15-150: Principles of Functional Programming</OutsideLink>. I was also a teaching assistant for <OutsideLink href="https://www.cs.cmu.edu/~15122/">15-122: Principles of Imperative Computation</OutsideLink> last semester as well.
+              The Computer Science topics that interest me the most currently are web development, programming languages, and computer systems. Outside of class, I enjoy gaming and relaxing.
             </ContentCard>
             <ContentCard>
-              I really enjoy helping people, and many of my projects are designed to help people with certain tasks. Over the last summer, I helped pioneer a website that taught the basics of web development for a web development class at my high school. Additionally, during my senior year, I worked on a project that improved the school's course catalog to aid students in their course selection process. Also, I volunteered as a beginning Java teacher at my local Chinese school and taught introductory Java topics to middle school students every week for a year.
+              Some of the classes that I have taken include (with the semester I took them in parentheses):
+              <ul>
+                <li><OutsideLink href="https://www.math.cmu.edu/~jmackey/151_128/welcome.html">Mathematical Foundations in Computer Science</OutsideLink> (Fall 2022)</li>
+                <li><OutsideLink href="https://www.cs.cmu.edu/~15122/">Principles of Imperative Computation</OutsideLink> (Fall 2022)</li>
+                <li><OutsideLink href="http://www.cs.cmu.edu/~15150/">Principles of Functional Programming</OutsideLink> (Spring 2023)</li>
+                <li><OutsideLink href="https://www.cs.cmu.edu/~15281/">Artificial Intelligence: Representation and Problem Solving</OutsideLink> (Spring 2023)</li>
+                <li><OutsideLink href="https://s23.cs251.com/">Great Ideas in Theoretical Computer Science</OutsideLink> (Spring 2023)</li>
+              </ul>
             </ContentCard>
             <ContentCard>
-              I graduated from Thomas Jefferson High School for Science and Technology in 2022 with a 4.51 GPA. In high school, I took many advanced courses including Multivariable Calculus, Linear Algebra, AP Computer Science with Data Structures, Artificial Intelligence, Machine Learning, Advanced Math Techniques, and AP Physics C. During my senior year, I was the statisician for my school's <Themed.a href="https://tjcsec.club/" target='_blank' rel='noopener noreferrer'>Computer Security Club</Themed.a> and helped host <Themed.a href="https://tjctf.org/" target='_blank' rel='noopener noreferrer'>TJCTF</Themed.a>, a CTF consisting of over 1100 players and over 300 teams. Additionally, I marched trumpet for my school's marching band for all four years.
+              I really enjoy helping people, and many of my projects are designed to help people with certain tasks. A couple summers ago, I helped pioneer a website that taught the basics of web development for a web development class at my high school. Additionally, during my senior year, I worked on a project that improved the school's course catalog to aid students in their course selection process. Also, I volunteered as a beginning Java teacher at my local Chinese school and taught introductory Java topics to middle school students every week for a year.
+            </ContentCard>
+            <ContentCard>
+              I graduated from Thomas Jefferson High School for Science and Technology in 2022 with a 4.51 GPA. In high school, I took many advanced courses including Multivariable Calculus, Linear Algebra, AP Computer Science with Data Structures, Artificial Intelligence, Machine Learning, Advanced Math Techniques, and AP Physics C. During my senior year, I was the statisician for my school's <OutsideLink href="https://tjcsec.club/">Computer Security Club</OutsideLink> and helped host <OutsideLink href="https://tjctf.org/">TJCTF</OutsideLink>, a CTF consisting of over 1100 players and over 300 teams. Additionally, I marched trumpet for my school's marching band for all four years and volunteered to be a quartermaster for three.
             </ContentCard>
             {/* <ContentCard
               sx={{
